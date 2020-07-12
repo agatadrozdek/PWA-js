bf70f826252b4cf8b4aefc4fd197847f2327fe83
ejmordo <aka.drozdie@gmail.com>
2020-07-12 10:04:22 +0200

    icons-fix1

--- sw.js
+++ sw.js
@@ -1,6 +1,7 @@
 var cacheName = "podyplomowe";
-var filesToCache = ["./", "./index.html", "./css/style.css", "./js/mai n.js"];
-/* Start the service worker and cache all of the app's content */ self.addEventListener("install", function (event) {
+var filesToCache = ["./", "./index.html", "./css/style.css", "./js/main.js"];
+/* Start the service worker and cache all of the app's content */ 
+self.addEventListener("install", function (event) {
     // Perform install steps
     event.waitUntil(
         caches.open(cacheName).then(function (cache) {
@@ -9,7 +10,8 @@ var filesToCache = ["./", "./index.html", "./css/style.css", "./js/mai n.js"];
         })
     );
 });
-/* Serve cached content when offline */ self.addEventListener("fetch", function (event) {
+/* Serve cached content when offline */ 
+self.addEventListener("fetch", function (event) {
     event.respondWith(
         caches.match(event.request).then(function (response) { // Cache hit - return response
             if (response) {
